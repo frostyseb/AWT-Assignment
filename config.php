4ce278b5cd87b53a508d1b242c028c50869f4b80
ming0520 <mingblog0520@gmail.com>
2019-01-21 12:36:25 +0800

    Add config.php
    Update client id and client secret

--- config.php
+++ config.php
@@ -1,7 +1,11 @@
 <?php
 
-session_start();
-require_once "API\GoogleAPI\vendor\autoload.php";
-$gClient = new Google_Client();
-$gClient->setClientId("");
-$gClient->setClientSecret("");
+    session_start();
+    require_once "API/GoogleAPI/vendor/autoload.php";
+    $gClient = new Google_Client();
+    $gClient->setClientId("333818657363-h92hbp9kkm5h3onnqqklhjnqt1pna3g9.apps.googleusercontent.com");
+    $gClient->setClientSecret("333818657363-h92hbp9kkm5h3onnqqklhjnqt1pna3g9.apps.googleusercontent.com");
+    $gClient->setApplicationName("CCA Login");
+    $gClient->setRedirectUri("http://localhost/AWT/AWT-Assignment/g-callback.php");
+    $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email ");
+?>
