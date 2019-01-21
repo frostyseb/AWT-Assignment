8ec99f8fa434250051b8cf33242bddaf7599789d
ming0520 <mingblog0520@gmail.com>
2019-01-21 14:37:36 +0800

    Update config.php secretkey

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
     require_once "API/GoogleAPI/vendor/autoload.php";
     $gClient = new Google_Client();
     $gClient->setClientId("333818657363-h92hbp9kkm5h3onnqqklhjnqt1pna3g9.apps.googleusercontent.com");
-    $gClient->setClientSecret("333818657363-h92hbp9kkm5h3onnqqklhjnqt1pna3g9.apps.googleusercontent.com");
+    $gClient->setClientSecret("K9hee3gITqI29iHVfZEAH8et");
     $gClient->setApplicationName("CCA Login");
     $gClient->setRedirectUri("http://localhost/AWT/AWT-Assignment/g-callback.php");
     $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email ");
