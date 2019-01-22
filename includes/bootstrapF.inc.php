6b95cb89070af1ec6d7bae1259f601a452618bc9
ming0520 <mingblog0520@gmail.com>
2019-01-22 09:35:32 +0800

    Create bootstrap?.inc.php

---
+++ includes/bootstrapF.inc.php
@@ -0,0 +1,15 @@
+<body>
+    	<!--jQuery first, then Bootstrap JS. -->
+	  <!--Load the CDN first-->
+	  <script src="http://code.jquery.com/jquery-3.3.1.min.js" 
+	  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" 
+	  crossorigin="anonymous"></script>
+	  <!-- If CDN failed to load, serve up the local version -->
+	  <script>
+	  	window.jQuery || 
+	  	document.write('<script src="vendor/node_modules/jquery/dist/jquery.js"><\/script>')
+	  </script>
+
+    <!-- Load jQuery --> 
+	<!--<script src="vendor/node_modules/initjQuery.js"></script>-->
+</body>
---
+++ includes/bootstrapH.inc.php
@@ -0,0 +1,16 @@
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!-- Google Api -->
+	  <!-- Font -->
+	  <link rel="stylesheet" type="text/css" 
+	  href="//fonts.googleapis.com/css?family=Raleway:400,800" />
+	<!-- Font awesome bootstrap-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+	 integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" 
+	 crossorigin="anonymous">
+	<!-- Bootstrap CSS and JS-->
+	<link rel="stylesheet" href="vendor/node_modules/bootstrap/dist/css/bootstrap.css">
+    <script src="vendor/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</head>
