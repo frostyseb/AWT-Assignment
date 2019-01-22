4758b30caf8724b65241f81145ddef16d1dd7506
frostyseb <46648408+frostyseb@users.noreply.github.com>
2019-01-23 00:10:02 +0800

    A lot of files
    Just for hexagon slideshow

---
+++ main_page.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	
+	<title>QIUP CCA Portal</title>
+	
+	<!--Bootstrap CSS-->
+	<link rel="stylesheet" href="tiles-transitions-slider/tiles-transitions-slider/example/octagon.css">
+
+</head>
+
+<body>
+
+	<div class="octagon" id="octagon">
+		<img src="src/assets/pic1.png">
+		<img src="src/assets/pic2.png">
+		<img src="src/assets/pic3.jpg">
+	</div>
+
+</body>
+
+<body>
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
+	<script src="tiles-transitions-slider/tiles-transitions-slider/jquery.tiles-slider.js"></script>
+	<script src="tiles-transitions-slider/tiles-transitions-slider/example/app.js"></script>
+	
+	
+</body>
+
+</html>
