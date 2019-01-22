c13544d1ca8e99e97cb155c6c126cec5a563e98c
ming0520 <mingblog0520@gmail.com>
2019-01-22 16:12:56 +0800

    Update g-callback.php
    Add function

--- g-callback.php
+++ g-callback.php
@@ -19,15 +19,33 @@
 
 	echo "<pre>";
     var_dump($userData);
+
     
-    $_SESSION['id'] = $userData['id'];
+
+	//Insert or update user data to the database
+    $userDataArray = array(
+        'oauth_provider'=> 'google',
+        'oauth_uid'     => $gpUserProfile['id'],
+        'first_name'    => $gpUserProfile['given_name'],
+        'last_name'     => $gpUserProfile['family_name'],
+        'email'         => $gpUserProfile['email'],
+        'gender'        => $gpUserProfile['gender'],
+        'locale'        => $gpUserProfile['locale'],
+        'picture'       => $gpUserProfile['picture'],
+        'link'          => $gpUserProfile['link']
+    );
+
+    $user = new Users();
+    $finalUserData = $user->checkUser($userDataArray);
+    
+   /* $_SESSION['id'] = $userData['id'];
     $_SESSION['email'] = $userData['email'];
     $_SESSION['gender'] = $userData['gender'];
     $_SESSION['picture'] = $userData['picture'];
     $_SESSION['familyName'] = $userData['familyName'];
-    $_SESSION['givenName'] = $userData['givenName'];
+    $_SESSION['givenName'] = $userData['givenName'];*/
 
-    header('Location: index.php');
-    exit();
+    //header('Location: index.php');
+    //exit();
 
 ?>
