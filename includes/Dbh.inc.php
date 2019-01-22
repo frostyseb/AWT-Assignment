c1a453f9f9e87b552456c81080cc5da9c1be3e20
ming0520 <mingblog0520@gmail.com>
2019-01-22 09:06:54 +0800

    Update dbh.inc.php
    added sever info

--- includes/Dbh.inc.php
+++ includes/Dbh.inc.php
@@ -1,6 +1,34 @@
 <?php
 
-    class Dbh(){
-        private $servername;
-        private
+class Dbh{
+    private $serverName;
+    private $userName;
+    private $password;
+    private $dbName;
+    private $charset;
+
+    private function __construct(){
+        $this->serverName = "localhost";
+        $this->userName ="root";
+        $this->password = "";
+        $this->dbName = "users";
+
+        $this->charset = "utf8mb4";
     }
+
+    protected function connect(){
+        
+        try{
+            //dsn = data source name
+            $dsn = "mysql:host=" . $this->serverName . ";dbname=" . $this->dbName .
+            ";charset=" . $this->charset;
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $pdo;
+        }catch(PDOException $e){
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+}
