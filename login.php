3667c94e44034579749bfa5cc20e260e35433854
frostyseb <46648408+frostyseb@users.noreply.github.com>
2019-01-23 10:31:30 +0800

    Update login.php
    Add hexagon slideshow

--- login.php
+++ login.php
@@ -15,8 +15,12 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	
 	<?php include 'includes/bootstrapH.inc.php'; ?>
-		<!--CSS Stylesheet-->
-		<link rel="stylesheet" href="src\css\styles.css">
+	
+	<!--Bootstrap CSS-->
+	<link rel="stylesheet" href="tiles-transitions-slider/example/octagon.css">
+	
+	<!--CSS Stylesheet-->
+	<link rel="stylesheet" href="src\css\styles.css">
 	
 	<!--Google font style-->
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab|Dosis" rel="stylesheet">
@@ -29,7 +33,13 @@
 		<h1 id="login_title">Quest International University Perak</h1>
 		<h2 id="login_portal">CCA Portal</h2>
 		<p class="form_element">Login now to discover upcoming CCA events & activities</p>
-		<img src="src\assets\photos.png" class="col-xs-12 cca_photos"><br><br>
+		
+		<div class="octagon" id="octagon">
+			<img src="src/assets/pic1.png">
+			<img src="src/assets/pic2.png">
+			<img src="src/assets/pic3.jpg">
+		</div>
+		
 		<button type="button" onclick="window.location = '<?php echo $loginURL ?>';" class="btn btn-outline-danger btn-block"><i class="fab fa-google"></i> SIGN IN WITH GOOGLE</button>
 	</div> <!-- END form_box -->
 	
@@ -40,6 +50,10 @@
 		});
 
 	</script>
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
+	<script src="tiles-transitions-slider/jquery.tiles-slider.js"></script>
+	<script src="tiles-transitions-slider/example/app.js"></script>
 	
 </body>
 <?php include 'includes/bootstrapF.inc.php'; ?>
