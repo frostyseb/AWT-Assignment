df0b456993a94a521322f41d0ec61f55d493daca
ming0520 <mingblog0520@gmail.com>
2019-01-23 11:29:10 +0800

    Updater user.inc.php
    Delete unnesssary function and modify function checkUser, add variable

--- includes/user.inc.php
+++ includes/user.inc.php
@@ -2,44 +2,57 @@
 
     class User extends Dbh{
 
-        public function getAllUsers () {
+        private $tableName =" users ";
+        private $userDataArray = array();
+
+        private $oProvider;
+        private $oUid;
+        private $fName;
+        private $lName;
+        private $email;
+        private $gender;
+        private $locale;
+        private $picture;
         
-            $stmt = $this->connect()->query("SELECT * FROM users");
-    
-            while($row = $stmt->fetch()) {
-               // var_dump($row);
-                $uid = $row['uid'];
-                return $uid;
-            }
+        public function storeNewUser (){
+            
         }
 
-        public function getUsersWithCountCheck() {
-            $id = 2;
-            $uid ="uid2";
-    
-            $stmt = $this->connect()->prepare("SELECT * FROM USERS WHERE id=? AND uid=?");
-            $stmt->execute([$id,$uid]);
-    
-            if($stmt->rowCount()){
-                while($row = $stmt->fetch()){
-                    return $row['uid'];
-                }
-    
-            }
-            else{
-                echo "getUsersWithCountCheck";
-            }
+        public function updateUser(){
+
         }
 
-        public function checkUser($userDataArray = array()){
+        //Automatically separate the data from array into variable
+        public function separateFromArray(){
+            $this->oProvider = $this->userDataArray['oProvider'];
+            $this->oUid = $this->userDataArray['oUid'];
+            $this->fName = $this->userDataArray['fName'];
+            $this->lName = $this->userDataArray['lName'];
+            $this->email = $this->userDataArray['email'];
+            $this->gender = $this->userDataArray['gender'];
+            $this->locale = $this->userDataArray['locale'];
+            $this->picture = $this->userDataArray['picture'];
+
+        }
+
+        //Set the user data array when checkUser() called
+        public function setUserDataArray($array = array()){
+            $this->userDataArray = $array;
+            separateFromArray();
+        }
+
+        //Entry point
+        public function checkUser($usdArray = array()){
+            setUserDataArray($usdArray);
+
             if(!empty($userDataArray)){
                 //Check the data is in the database or not
-                $prevQuery = "SELECT * FROM " .
-                "WHERE oauth_provider = ' " . $userDataArray['oauth_provider']. "'
-                AND oauth_uid = ' " . $userDataArray['oauth_uid'] . "'";
+                $prevQuery = "SELECT * FROM" . $this->tableName.
+                "WHERE oauth_provider = '" . $userDataArray['oauth_provider']. "'".
+                "AND oauth_uid = '" . $userDataArray['oauth_uid'] . "'";
                 
-                $stmt = $this->connect()->query($sql);
-                if($stmt->num_rows > 0){
+                $stmt = $this->connect()->query($prevQuery);
+                if($stmt->rowCount()){
                     echo "There is data";
                 }
                 else{
