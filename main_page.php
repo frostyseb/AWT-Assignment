f78f9d4cea86abb2c3a2a7b39d02ec03ae664fe0
ming0520 <mingblog0520@gmail.com>
2019-01-24 21:21:55 +0800

    add navbar and reposition footer boorstrap

--- main_page.php
+++ main_page.php
@@ -10,7 +10,7 @@
 	
 	<!--Bootstrap js-->
 	<?php include 'includes/bootstrapH.inc.php'; ?>
-	<?php include 'includes/bootstrapF.inc.php'; ?>
+
 	
 	<!--Google font style-->
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab|Dosis" rel="stylesheet">
@@ -50,7 +50,7 @@
 		}
 	</script>
 </head>
-
+<?php include 'simpleNav.php';?>
 <body>
 	<div id = "header">
 	<h1><p><a href="http://www.qiup.edu.my" target="_blank"><img src="src\assets\quest_logo.png"></a>QIUP CCA</p></h1>
@@ -90,7 +90,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	<script src="Touch-Swipe-Bootstrap-Carousel/dist/bootstrap-swipe-carousel.js"></script>
-
+	<?php include 'includes/bootstrapF.inc.php'; ?>
 	<script>
 		const carouselEl = $('.my-carousel');
 
