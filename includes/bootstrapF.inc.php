58ceaf8e4d9c1ff12a51dcd213d974cf0df9d3fd
ming0520 <mingblog0520@gmail.com>
2019-01-24 21:21:14 +0800

    Update bootstrapF.inc.php
    added few bootstrap for nav bar

--- includes/bootstrapF.inc.php
+++ includes/bootstrapF.inc.php
@@ -12,3 +12,7 @@
 
     <!-- Load jQuery --> 
 	<!--<script src="vendor/node_modules/initjQuery.js"></script>-->
+
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
