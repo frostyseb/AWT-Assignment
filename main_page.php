479087723fb31e0bf63f823e0b7cc2bdd42c04ec
ming0520 <mingblog0520@gmail.com>
2019-01-27 12:34:31 +0800

    Beautify

--- main_page.php
+++ main_page.php
@@ -5,22 +5,23 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	
+
 	<title>QIUP CCA Portal</title>
-	
+
 	<!--Bootstrap js-->
 	<?php include 'includes/bootstrapH.inc.php'; ?>
 
 	<!--Google font style-->
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab|Indie+Flower|Comfortaa|Special+Elite" rel="stylesheet">
-	
+
 	<!--Bootstrap CSS for slideshow-->
 	<link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+	 crossorigin="anonymous">
 
 	<!--CSS Stylesheet-->
 	<link rel="stylesheet" href="src/css/styles_main_page.css" type="text/css">
-	
+
 </head>
 
 <?php include 'simpleNav.php';?>
@@ -31,10 +32,11 @@
 	<!--Facebook API-->
 	<div id="fb-root"></div>
 	<script>
-		(function(d, s, id) {
+		(function (d, s, id) {
 			var js, fjs = d.getElementsByTagName(s)[0];
 			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
+			js = d.createElement(s);
+			js.id = id;
 			js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2';
 			fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
@@ -47,41 +49,41 @@
 
 	<!--News Carousel-->
 	<div id="news" class="carousel slide my-carousel" data-ride="carousel">
-	<ol class="carousel-indicators">
-	<li data-target="#news" data-slide-to="0" class="active"></li>
-	<li data-target="#news" data-slide-to="1" class=""></li>
-	<li data-target="#news" data-slide-to="2" class=""></li>
-	<li data-target="#news" data-slide-to="3" class=""></li>
-	<li data-target="#news" data-slide-to="4" class=""></li>
-	</ol>
-	<div class="carousel-inner">
-	<div class="carousel-item active">
-		<div id="news-item">
-			<b>BREAKING NEWS</b>
-			<p>Free CCA hours for everybody</p>
-		</div>
-	</div> <!-- END carousel-item active -->
-	<div class="carousel-item">
-		<div id="news-item">Insert news here</div>
-	</div> <!-- END carousel-item -->
-	<div class="carousel-item">
-		<div id="news-item">Insert news here</div>
-	</div> <!-- END carousel-item -->
-	<div class="carousel-item">
-		<div id="news-item">Insert news here</div>
-	</div> <!-- END carousel-item -->
-	<div class="carousel-item">
-		<div id="news-item">Insert news here</div>
-	</div> <!-- END carousel-item -->
-	</div> <!-- END carousel-inner -->
-	<a class="carousel-control-prev" href="#news" role="button" data-slide="prev">
-	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-	<span class="sr-only">Previous</span>
-	</a>
-	<a class="carousel-control-next" href="#news" role="button" data-slide="next">
-	<span class="carousel-control-next-icon" aria-hidden="true"></span>
-	<span class="sr-only">Next</span>
-	</a>
+		<ol class="carousel-indicators">
+			<li data-target="#news" data-slide-to="0" class="active"></li>
+			<li data-target="#news" data-slide-to="1" class=""></li>
+			<li data-target="#news" data-slide-to="2" class=""></li>
+			<li data-target="#news" data-slide-to="3" class=""></li>
+			<li data-target="#news" data-slide-to="4" class=""></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<div id="news-item">
+					<b>BREAKING NEWS</b>
+					<p>Free CCA hours for everybody</p>
+				</div>
+			</div> <!-- END carousel-item active -->
+			<div class="carousel-item">
+				<div id="news-item">Insert news here</div>
+			</div> <!-- END carousel-item -->
+			<div class="carousel-item">
+				<div id="news-item">Insert news here</div>
+			</div> <!-- END carousel-item -->
+			<div class="carousel-item">
+				<div id="news-item">Insert news here</div>
+			</div> <!-- END carousel-item -->
+			<div class="carousel-item">
+				<div id="news-item">Insert news here</div>
+			</div> <!-- END carousel-item -->
+		</div> <!-- END carousel-inner -->
+		<a class="carousel-control-prev" href="#news" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#news" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
 	</div> <!-- END carousel -->
 
 	<br><br>
@@ -89,55 +91,63 @@
 
 	<!-- Events Carousel -->
 	<div id="events" class="carousel slide my-carousel" data-ride="carousel">
-	  <ol class="carousel-indicators">
-		<li data-target="#events" data-slide-to="0" class="active"></li>
-		<li data-target="#events" data-slide-to="1" class=""></li>
-		<li data-target="#events" data-slide-to="2" class=""></li>
-	  </ol>
-	  <div class="carousel-inner">
-		<div class="carousel-item active">
-		  <img class="d-block w-100" src="src/assets/pic1.png" alt="First slide">
-		</div> <!-- END carousel-item active -->
-		<div class="carousel-item">
-		  <img class="d-block w-100" src="src/assets/pic2.png" alt="Second slide">
-		</div> <!-- END carousel-item -->
-		<div class="carousel-item">
-		  <img class="d-block w-100" src="src/assets/pic3.png" alt="Third slide">
-		</div> <!-- END carousel-item -->
-	  </div> <!-- END carousel-inner -->
-	  <a class="carousel-control-prev" href="#events" role="button" data-slide="prev">
-		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-		<span class="sr-only">Previous</span>
-	  </a>
-	  <a class="carousel-control-next" href="#events" role="button" data-slide="next">
-		<span class="carousel-control-next-icon" aria-hidden="true"></span>
-		<span class="sr-only">Next</span>
-	  </a>
+		<ol class="carousel-indicators">
+			<li data-target="#events" data-slide-to="0" class="active"></li>
+			<li data-target="#events" data-slide-to="1" class=""></li>
+			<li data-target="#events" data-slide-to="2" class=""></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="src/assets/pic1.png" alt="First slide">
+			</div> <!-- END carousel-item active -->
+			<div class="carousel-item">
+				<img class="d-block w-100" src="src/assets/pic2.png" alt="Second slide">
+			</div> <!-- END carousel-item -->
+			<div class="carousel-item">
+				<img class="d-block w-100" src="src/assets/pic3.png" alt="Third slide">
+			</div> <!-- END carousel-item -->
+		</div> <!-- END carousel-inner -->
+		<a class="carousel-control-prev" href="#events" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#events" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
 	</div> <!-- END events -->
-	
+
 	<!--Bootstrap js for slideshow-->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+	 crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+	 crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+	 crossorigin="anonymous"></script>
 	<script src="Touch-Swipe-Bootstrap-Carousel/dist/bootstrap-swipe-carousel.js"></script>
-	
-	<div id = "header">
+
+	<div id="header">
 		<div class='m'>
 			<br>
-			<font face="Indie Flower"><strong><marquee scrollamount="10">! ! WELCOME TO THE QIUP CCA WEBSITE ! !</marquee></strong></font>
+			<font face="Indie Flower"><strong>
+					<marquee scrollamount="10">! ! WELCOME TO THE QIUP CCA WEBSITE ! !</marquee>
+				</strong></font>
 		</div> <!-- END m -->
 	</div> <!-- END header -->
-	
+
 	<div class="fbAPI">
 		<br>
-		<h4>Check out the latest CCA activities or <br>events from  <i class="fa fa-facebook-square" style="color:darkblue;"></i> QIUP Facebook!</h4>
-		<div class="fb-page" data-href="https://www.facebook.com/QuestInternationalUniversityPerak/" data-tabs="timeline" data-width="500" data-height="1270" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false">
+		<h4>Check out the latest CCA activities or <br>events from <i class="fa fa-facebook-square" style="color:darkblue;"></i>
+			QIUP Facebook!</h4>
+		<div class="fb-page" data-href="https://www.facebook.com/QuestInternationalUniversityPerak/" data-tabs="timeline"
+		 data-width="500" data-height="1270" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false"
+		 data-show-facepile="false">
 			<blockquote cite="https://www.facebook.com/QuestInternationalUniversityPerak/" class="fb-xfbml-parse-ignore">
 				<a href="https://www.facebook.com/QuestInternationalUniversityPerak/">Quest International University Perak (QIUP)</a>
 			</blockquote>
 		</div> <!-- END fb-page -->
 	</div> <!-- END fb-api -->
-	
+
 	<br>
 	<?php
 	echo "<div class='clubSoc'><h4 class='ev'>Clubs & Societies</h4>";
@@ -155,39 +165,39 @@
 
 	<script>
 		$('.list-item')
-    	.on('mouseenter', function(){
-        	var div = $(this);
-        	div.stop(true, true).animate({ 
-            width: "62%",
-            height: "50%",
-			padding: "1.3em"
-        	}, 'fast');
-    	})
-    	.on('mouseleave', function(){
-        	var div = $(this);
-        	div.stop(true, true).animate({ 
-            width: "60%",
-            height: "20%",
-			padding: "1em"
-        	}, 'fast');
-    	})
+			.on('mouseenter', function () {
+				var div = $(this);
+				div.stop(true, true).animate({
+					width: "62%",
+					height: "50%",
+					padding: "1.3em"
+				}, 'fast');
+			})
+			.on('mouseleave', function () {
+				var div = $(this);
+				div.stop(true, true).animate({
+					width: "60%",
+					height: "20%",
+					padding: "1em"
+				}, 'fast');
+			})
 	</script>
-	
+
 	<?php include 'includes/bootstrapF.inc.php'; ?>
-	
+
 </body>
 
 <script>
-$(document).ready(
-	function() {
-		$(".navbar-collapse").hide();
-	}
-)
-$(".navbar-toggler-icon").click(
-    function() {
-        $(".navbar-collapse").toggle(500).delay(200);
-    }
-)
+	$(document).ready(
+		function () {
+			$(".navbar-collapse").hide();
+		}
+	)
+	$(".navbar-toggler-icon").click(
+		function () {
+			$(".navbar-collapse").toggle(500).delay(200);
+		}
+	)
 </script>
 
 </html>
--- simpleNav.php
+++ simpleNav.php
@@ -1,8 +1,10 @@
 <script src="src/js/validation.js"></script>
 
 <nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
-  <a href="http://www.qiup.edu.my" target="_blank"><img src="src/assets/quest_logo.png" width="80em" height="80em"></a><a class="navbar-brand" href="main_page.php">CCA QIUP</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <a href="http://www.qiup.edu.my" target="_blank"><img src="src/assets/quest_logo.png" width="80em" height="80em"></a><a
+    class="navbar-brand" href="main_page.php">CCA QIUP</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
@@ -13,23 +15,23 @@
           <i class="fa fa-home"></i>
           Home
           <span class="sr-only">(current)</span>
-          </a>
+        </a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="login.php">
           <i class="fa fa-sign-in"></i>
           Login
           <span class="sr-only">(current)</span>
-          </a>
+        </a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="index.php">
           <i class="fa fa-user-circle-o"></i>
           Profile
           <span class="sr-only">(current)</span>
-          </a>
+        </a>
       </li>
-     <!-- <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link" href="#">
           <i class="fa fa-envelope-o">
             <span class="badge badge-danger">11</span>
@@ -64,189 +66,192 @@
         </a>
       </li>
     </ul>
-	<form id="searchForm" autocomplete="off" onsubmit="return validation()" method="POST" name="searchForm" action="search.php">
-		<div class="autocomplete" style="width:300px;">
-			<input id="myInput" type="text" name="clubs" placeholder="Search clubs or societies" onkeyup="displayHint(this.value)" required> 
-		</div>
-		<button type="submit" form="searchForm" id="searchBtn"><i class="fa fa-search"></i></button>
-		<div id="hintText"></div>
-	</form>
-	
-	<script src="src/js/suggest.js"></script>
-	
+    <form id="searchForm" autocomplete="off" onsubmit="return validation()" method="POST" name="searchForm" action="search.php">
+      <div class="autocomplete" style="width:300px;">
+        <input id="myInput" type="text" name="clubs" placeholder="Search clubs or societies" onkeyup="displayHint(this.value)"
+          required>
+      </div>
+      <button type="submit" form="searchForm" id="searchBtn"><i class="fa fa-search"></i></button>
+      <div id="hintText"></div>
+    </form>
+
+    <script src="src/js/suggest.js"></script>
+
   </div>
 </nav>
 
 <style>
-@import url("//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
-.navbar-icon-top .navbar-nav .nav-link > .fa {
-  position: relative;
-  width: 36px;
-  font-size: 24px;
-}
+  @import url("//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
 
-.navbar-icon-top .navbar-nav .nav-link > .fa > .badge {
-  font-size: 0.75rem;
-  position: absolute;
-  right: 0;
-  font-family: sans-serif;
-}
-
-.navbar-icon-top .navbar-nav .nav-link > .fa {
-  top: 3px;
-  line-height: 12px;
-}
-
-.navbar-icon-top .navbar-nav .nav-link > .fa > .badge {
-  top: -10px;
-}
-
-@media (min-width: 576px) {
-  .navbar-icon-top.navbar-expand-sm .navbar-nav .nav-link {
-    text-align: center;
-    display: table-cell;
-    height: 70px;
-    vertical-align: middle;
-    padding-top: 0;
-    padding-bottom: 0;
+  .navbar-icon-top .navbar-nav .nav-link>.fa {
+    position: relative;
+    width: 36px;
+    font-size: 24px;
   }
 
-  .navbar-icon-top.navbar-expand-sm .navbar-nav .nav-link > .fa {
+  .navbar-icon-top .navbar-nav .nav-link>.fa>.badge {
+    font-size: 0.75rem;
+    position: absolute;
+    right: 0;
+    font-family: sans-serif;
+  }
+
+  .navbar-icon-top .navbar-nav .nav-link>.fa {
+    top: 3px;
+    line-height: 12px;
+  }
+
+  .navbar-icon-top .navbar-nav .nav-link>.fa>.badge {
+    top: -10px;
+  }
+
+  @media (min-width: 576px) {
+    .navbar-icon-top.navbar-expand-sm .navbar-nav .nav-link {
+      text-align: center;
+      display: table-cell;
+      height: 70px;
+      vertical-align: middle;
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+
+    .navbar-icon-top.navbar-expand-sm .navbar-nav .nav-link>.fa {
+      display: block;
+      width: 48px;
+      margin: 2px auto 4px auto;
+      top: 0;
+      line-height: 24px;
+    }
+
+    .navbar-icon-top.navbar-expand-sm .navbar-nav .nav-link>.fa>.badge {
+      top: -7px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .navbar-icon-top.navbar-expand-md .navbar-nav .nav-link {
+      text-align: center;
+      display: table-cell;
+      height: 70px;
+      vertical-align: middle;
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+
+    .navbar-icon-top.navbar-expand-md .navbar-nav .nav-link>.fa {
+      display: block;
+      width: 48px;
+      margin: 2px auto 4px auto;
+      top: 0;
+      line-height: 24px;
+    }
+
+    .navbar-icon-top.navbar-expand-md .navbar-nav .nav-link>.fa>.badge {
+      top: -7px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .navbar-icon-top.navbar-expand-lg .navbar-nav .nav-link {
+      text-align: center;
+      display: table-cell;
+      height: 70px;
+      vertical-align: middle;
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+
+    .navbar-icon-top.navbar-expand-lg .navbar-nav .nav-link>.fa {
+      display: block;
+      width: 48px;
+      margin: 2px auto 4px auto;
+      top: 0;
+      line-height: 24px;
+    }
+
+    .navbar-icon-top.navbar-expand-lg .navbar-nav .nav-link>.fa>.badge {
+      top: -7px;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    .navbar-icon-top.navbar-expand-xl .navbar-nav .nav-link {
+      text-align: center;
+      display: table-cell;
+      height: 70px;
+      vertical-align: middle;
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+
+    .navbar-icon-top.navbar-expand-xl .navbar-nav .nav-link>.fa {
+      display: block;
+      width: 48px;
+      margin: 2px auto 4px auto;
+      top: 0;
+      line-height: 24px;
+    }
+
+    .navbar-icon-top.navbar-expand-xl .navbar-nav .nav-link>.fa>.badge {
+      top: -7px;
+    }
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .autocomplete {
+    /*the container must be positioned relative:*/
+    position: relative;
+    display: inline-block;
+  }
+
+  input {
+    border: 1px solid transparent;
+    background-color: #f1f1f1;
+    padding: 10px;
+    font-size: 16px;
+  }
+
+  input[type=text] {
+    background-color: #f1f1f1;
+    width: 100%;
+  }
+
+  #searchBtn {
+    background-color: DodgerBlue;
+    color: #fff;
+    padding: 10px;
+    border: 1px solid DodgerBlue;
+    cursor: pointer;
+  }
+
+  .autocompleteItems {
     display: block;
-    width: 48px;
-    margin: 2px auto 4px auto;
-    top: 0;
-    line-height: 24px;
+    border: none;
+    position: relative;
+    padding: 1em;
+    background-color: #D1D1D1;
+    cursor: pointer;
+    width: 100%;
+    border-bottom: 1px solid #A2A2A2;
+    z-index: 999;
   }
 
-  .navbar-icon-top.navbar-expand-sm .navbar-nav .nav-link > .fa > .badge {
-    top: -7px;
-  }
-}
-
-@media (min-width: 768px) {
-  .navbar-icon-top.navbar-expand-md .navbar-nav .nav-link {
-    text-align: center;
-    display: table-cell;
-    height: 70px;
-    vertical-align: middle;
-    padding-top: 0;
-    padding-bottom: 0;
+  #hintText {
+    position: absolute;
   }
 
-  .navbar-icon-top.navbar-expand-md .navbar-nav .nav-link > .fa {
-    display: block;
-    width: 48px;
-    margin: 2px auto 4px auto;
-    top: 0;
-    line-height: 24px;
+
+  .autocompleteItems :hover {
+    /*when hovering an item:*/
+    background-color: #e9e9e9;
   }
 
-  .navbar-icon-top.navbar-expand-md .navbar-nav .nav-link > .fa > .badge {
-    top: -7px;
+  .autocompleteItems :focus {
+    /*when navigating through the items using the arrow keys:*/
+    background-color: DodgerBlue !important;
+    color: #ffffff;
   }
-}
-
-@media (min-width: 992px) {
-  .navbar-icon-top.navbar-expand-lg .navbar-nav .nav-link {
-    text-align: center;
-    display: table-cell;
-    height: 70px;
-    vertical-align: middle;
-    padding-top: 0;
-    padding-bottom: 0;
-  }
-
-  .navbar-icon-top.navbar-expand-lg .navbar-nav .nav-link > .fa {
-    display: block;
-    width: 48px;
-    margin: 2px auto 4px auto;
-    top: 0;
-    line-height: 24px;
-  }
-
-  .navbar-icon-top.navbar-expand-lg .navbar-nav .nav-link > .fa > .badge {
-    top: -7px;
-  }
-}
-
-@media (min-width: 1200px) {
-  .navbar-icon-top.navbar-expand-xl .navbar-nav .nav-link {
-    text-align: center;
-    display: table-cell;
-    height: 70px;
-    vertical-align: middle;
-    padding-top: 0;
-    padding-bottom: 0;
-  }
-
-  .navbar-icon-top.navbar-expand-xl .navbar-nav .nav-link > .fa {
-    display: block;
-    width: 48px;
-    margin: 2px auto 4px auto;
-    top: 0;
-    line-height: 24px;
-  }
-
-  .navbar-icon-top.navbar-expand-xl .navbar-nav .nav-link > .fa > .badge {
-    top: -7px;
-  }
-}
-
-* { 
-	box-sizing: border-box; 
-}
-
-.autocomplete {
-  /*the container must be positioned relative:*/
-  position: relative;
-  display: inline-block;
-}
-
-input {
-  border: 1px solid transparent;
-  background-color: #f1f1f1;
-  padding: 10px;
-  font-size: 16px;
-}
-input[type=text] {
-  background-color: #f1f1f1;
-  width: 100%;
-}
-
-#searchBtn {
-  background-color: DodgerBlue;
-  color: #fff;
-  padding:10px;
-  border:1px solid DodgerBlue;
-  cursor:pointer;
-}
-
-.autocompleteItems {
-  display:block;
-  border:none;
-  position:relative;
-  padding:1em;
-  background-color:#D1D1D1;
-  cursor:pointer;
-  width:100%;
-  border-bottom:1px solid #A2A2A2;
-  z-index:999;
-}
-
-#hintText{
-  position:absolute;
-}
-
-
-.autocompleteItems :hover {
-  /*when hovering an item:*/
-  background-color: #e9e9e9; 
-}
-
-.autocompleteItems :focus {
-  /*when navigating through the items using the arrow keys:*/
-  background-color: DodgerBlue !important; 
-  color: #ffffff; 
-}
 </style>
