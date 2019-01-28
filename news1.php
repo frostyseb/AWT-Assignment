82ba6bb70c5843bc98cd1e132bc346af9323318d
frostyseb <46648408+frostyseb@users.noreply.github.com>
2019-01-28 21:43:00 +0800

    Add news1.php

---
+++ news1.php
@@ -0,0 +1,69 @@
+<html>
+
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	
+	<title>Conference on Digital Marketing and Entrepreneurship 2019</title>
+	
+	<!--Bootstrap js-->
+	<?php include 'includes/bootstrapH.inc.php'; ?>
+	
+	<!--Title Bar Icon-->
+	<link rel="icon" type="image/ico" href="src/assets/quest_logo.png">
+	
+	<!--Google font style-->
+	<link href="https://fonts.googleapis.com/css?family=Alegreya|Exo|KoHo" rel="stylesheet">
+	
+	<!--CSS Stylesheets-->
+	<link rel="stylesheet" href="src/css/styles_news.css" type="text/css">
+
+</head>
+
+<?php include 'simpleNav.php';?>
+
+<body>
+	<div class="conference">
+		<h1>Conference on Digital Marketing and Entrepreneurship 2019</h1>
+		<br>
+		<p class="pimg"><img src="src/assets/conference.jpg"></p>
+		<div class="para">
+			<p>
+				Quest International University Perak is hosting its first ever conference on Digital Marketing and Entrepreneurship leaded by several experienced speakers.
+				The speaker include <i><b>Marcus Teoh</b>, Facebook Marketer, <b>Noor Aimi Mohd Yusoff</b>, Founder and Director of Sahi Elements Sdn. Bhd., <b>Felicia Yeow</b>, 
+				Founder of Carver's Homemade Nut Butters, <b>Mandy Leong</b>, Founder of Chriszen,</i> and <i><b>Cheong Mei Mei</b>, Business Director of Wavemaker</i>. Come 
+				engage and learn how to be a successful entrepreneur with them, and also find out their way of acheiving groundbreaking success.<br><br>
+				Details are as follows:-
+				<ul>
+					<li>Date: 24th January 2019</li>
+					<li>Time: 9:00AM - 4:00PM</li>
+					<li>Venue: QIU Auditorium, Level 3 @ TTS</li>
+				</ul>
+				See you there!
+			</p>
+		</div><!--END para-->
+	</div><!--END conference-->
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			$(".conference").fadeIn(1500);
+		});
+	</script>
+	
+	<div class="footer">
+		<h4>Stay Connected</h4>
+		<div class="links">
+			<a href="https://www.facebook.com/QuestInternationalUniversityPerak/" target="_blank"><i class="fa fa-facebook-square"></i></a>
+			<a href="https://twitter.com/qiup_edu?lang=en"  target="_blank"><i class="fa fa-twitter-square"></i></a>
+			<a href="https://www.instagram.com/qiup_edu/?hl=en"  target="_blank"><i class="fa fa-instagram"></i></a>
+			<a href="http://www.qiup.edu.my/" target="_blank"><i class="fa fa-globe" ></i></a>
+			<a href="mailto:enquiries@qiup.edu.my" target="_blank"><i class="fa fa-envelope-o"></i></a>
+			<a href="tel:+6052490500" target="_blank"><i class="fa fa-phone"></i></a>
+		</div> <!-- END links -->
+		<p>Copyright 2019 &copy; Tan, Seb, Jason, Reshmi</p>
+	</div> <!-- END footer -->
+	
+</body>
