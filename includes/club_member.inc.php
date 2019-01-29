453e458c9914e0f886ecb49490d4e4dc90db5956
ming0520 <mingblog0520@gmail.com>
2019-01-30 06:37:56 +0800

    Update club_member.php
    add function show member list @important

--- includes/club_member.inc.php
+++ includes/club_member.inc.php
@@ -9,20 +9,37 @@
         private $dataArray = array();
 
         public function checkClubMember($dataArray){
-            if(!empty($dataArray)){
                 $this->dataArray = $dataArray;
                 $this->separateDataArray();
-
-                $stmt->connect()->prepare("INSERT INTO club_member SET student_id=?, club_id=?");
+                $stmt = $this->connect()->prepare("SELECT * FROM club_member WHERE student_id=? AND club_id=?");
                 $stmt->execute([$this->student_id,$this->club_id]);
+
+                if(!$stmt->rowCount()){
+                    $stmt = $this->connect()->prepare("INSERT INTO club_member SET student_id=?, club_id=?");
+                    $stmt->execute([$this->student_id,$this->club_id]);
+                }
+                else{
+                    echo "Repeated Data";
+                }
+
+                
+        }
+
+        public function fetchByClub($clubId){
+            $stmt = $this->connect()->prepare("SELECT * FROM club_member WHERE club_id=?");
+            $stmt->execute([$clubId]);
+            if($stmt->rowCount()){
+                while($row = $stmt->fetch()){
+                    $student_id[] = $row['student_id'];
+                }
+                return $student_id;
+    
             }
-        }
-
-        public function fetchAllData(){
-            $stmt = $this->connect()->prepare("SELECT * FROM club_member WHERE student_id=?");
-            $stmt->execute([$this->student_id]);
-        }
-
+            else{
+                echo "fetchByClub";
+                return NULL;
+            }
+    }
         public function separateDataArray(){
             $this->student_id = $this->dataArray["student_id"];
             $this->club_id = $this->dataArray["club_id"];
